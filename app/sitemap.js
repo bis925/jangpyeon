624d93ce377d736d43098662e426ee03602a3c35
bis925 <83548331+bis925@users.noreply.github.com>
2026-08-08 13:13:20 +0900

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,10 @@
-
+export default function sitemap() {
+  return [
+    {
+      url: 'https://jangpyeon.kr',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
