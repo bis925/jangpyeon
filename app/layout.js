9b59cc25f06f45f1607e541b76e57742cbcfa58e
bis925 <83548331+bis925@users.noreply.github.com>
2026-08-08 13:20:44 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ export const metadata = {
   description: "휠체어·유모차 접근성, 장애인 화장실 정보를 함께 기록하는 서비스",
   other: {
     "naver-site-verification": "e53320c92097dc0b07b53181135a5269acb724e4",
+    "google-site-verification": "lbr7goiG8RIJz87vb4T3RSeIV5rkh4F3gWmXXdA1tUw",
   },
 };
 
