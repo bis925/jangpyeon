fcb3c9229c5c6b0ccb031e178fbe2e5eaef06cc0
bis925 <83548331+bis925@users.noreply.github.com>
2026-09-07 09:22:11 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@codetrix-studio/capacitor-google-auth": false,
+    };
+    return config;
+  },
+};
 module.exports = nextConfig;
