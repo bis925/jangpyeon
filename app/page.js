e57ec864319546c2fc3aac4e41dd9163a2e59c42
bis925 <83548331+bis925@users.noreply.github.com>
2026-09-09 15:45:28 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import dynamic from "next/dynamic";
 import "react-quill-new/dist/quill.snow.css";
 import { supabase } from "../lib/supabaseClient";
 
-const ReactQuill = dynamic(() => import {
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+import {
   Search, MapPin, Plus, User, Check, ChevronRight,
     Accessibility, DoorOpen, Baby, MoveVertical, Sparkles, X, Star, LogOut, Mail, Camera, Pencil, Megaphone, ShieldCheck, Paperclip, Bold, MessageCircle, Headset, Italic, Underline, Highlighter, Link2, Locate, LocateFixed, Trash2, Clipboard, ZoomIn, ZoomOut, Type, Navigation, Flag, Bell, Gift, Phone, MessageSquare, Heart, CheckCircle, Palette, Mic,
 } from "lucide-react";
